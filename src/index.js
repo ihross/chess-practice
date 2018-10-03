ec7a972b91511a7780170719f445bebe63745e53
Ian Ross <ianhowesross@gmail.com>
2018-10-03 17:19:58 -0400

    Removed extra spaces

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return ( 
-            <button className = "square"> 
+            <button className="square"> 
                 { /* TODO */ } 
             </button>
         );
@@ -22,19 +22,19 @@ class Board extends React.Component {
 
         return (
             <div>
-                <div className = "status">{status}</div> 
-                <div className = "board-row"> 
+                <div className="status">{status}</div> 
+                <div className="board-row"> 
                     {this.renderSquare(0)} 
                     {this.renderSquare(1)} 
                     {this.renderSquare(2)} 
                 </div> 
-                <div className = "board-row"> 
+                <div className="board-row"> 
                     {this.renderSquare(3)} 
                     {this.renderSquare(4)} 
                     {this.renderSquare(5)} 
                 </div> 
             
-                <div className = "board-row" > 
+                <div className="board-row" > 
                     {this.renderSquare(6)} 
                     {this.renderSquare(7)} 
                     {this.renderSquare(8)} 
@@ -47,11 +47,11 @@ class Board extends React.Component {
 class Game extends React.Component {
     render() {
         return (
-            <div className = "game">
-                <div className = "game-board">
+            <div className="game">
+                <div className="game-board">
                     <Board />
                 </div> 
-                <div className = "game-info">
+                <div className="game-info">
                      <div>{ /* status */}</div> 
                      <ol>{ /* TODO */}</ol> 
                 </div> 
